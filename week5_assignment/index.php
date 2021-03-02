5ceaa40036d63c137b102a1da1559b495e8e88e6
mark390 <55713492+mark390@users.noreply.github.com>
2021-03-02 09:49:57 -0500

    Update index.php

--- week5_assignment/index.php
+++ week5_assignment/index.php
@@ -67,49 +67,5 @@
             $todoitems = get_todo_items();
             include('view/list_categories.php');
 
-
-
     }
     
-
-    
-    //DELETE ITEM FROM DB//
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ToDo List</title>
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-</head>
-<body>
-    <h1>ToDo List</h1>
-<section>
-    <ul>
-        <?php foreach ($ItemNum as $i) : ?>
-            <li>
-                <?php echo $i['Title']; ?>
-                <?php echo $i['Description'] ?>
-                <form action="model/delete.php" method="POST">Delete</form>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-</section>
-    <h2>Add Item</h2>
-    <form class="submitform" action="." method=POST>
-        <label for="item">Item:></label>
-        <input type="text" id="item" name="Item" required>
-        <label for="itemdesc">Item Description:></label>
-        <input type="text" id="itemdesc" name="Item Description" required>
-        <button>Submit<button>
-    </form>
-</section>
-
-    <!-- <i class="material-icons">delete</i> -->
-    
-</body>
-</html>
